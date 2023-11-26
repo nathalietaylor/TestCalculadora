70df0599dd888b5c84c6d6670dd29f505da912c2
katewitkoski <ka.lwitkoski@gmail.com>
2023-11-26 20:31:33 -0300

    tira os imports desnecessarios

--- src/test/java/CalculadoraOnlineSomaTest.java
+++ src/test/java/CalculadoraOnlineSomaTest.java
@@ -2,8 +2,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
